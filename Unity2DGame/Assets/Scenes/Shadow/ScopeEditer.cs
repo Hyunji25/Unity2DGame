119d5d1ca8c68b70a3685d9579ba80214310e616
Hyunji <chj09255@gmail.com>
2023-04-25 21:45:54 +0900

    tfu

---
+++ Unity2DGame/Assets/Scenes/Shadow/ScopeEditer.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScopeController))]
+public class ScopeEditer : Editor
+{
+    private void OnSceneGUI()
+    {
+        ScopeController targetCompnemt = (ScopeController)target;
+
+        float Angle = targetCompnemt.Angle * 0.5f;
+
+
+        Handles.DrawWireArc(targetCompnemt.transform.position, Vector3.up, Vector3.forward, 360.0f, targetCompnemt.radius);
+
+        float Segments = targetCompnemt.Angle / targetCompnemt.Segments;
+
+        for (int i = 0; i < targetCompnemt.Segments + 1; ++i)
+        {
+            Vector3 angleLeftPoint = new Vector3(
+            Mathf.Sin(-Segments * i * Mathf.Deg2Rad),
+            0.0f,
+            Mathf.Cos(-Segments * i * Mathf.Deg2Rad));
+
+            Vector3 angleRightPoint = new Vector3(
+            Mathf.Sin(Segments * i * Mathf.Deg2Rad),
+            0.0f,
+            Mathf.Cos(Segments * i * Mathf.Deg2Rad));
+
+            Handles.DrawLine(targetCompnemt.transform.position,
+            targetCompnemt.transform.position + angleLeftPoint * targetCompnemt.radius);
+
+            Handles.DrawLine(targetCompnemt.transform.position,
+            targetCompnemt.transform.position + angleRightPoint * targetCompnemt.radius);
+        }
+    }
+}
